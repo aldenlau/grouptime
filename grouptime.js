9d329deb876c198bbf9f7afd2d98ee42c5195722
Alden Lau <aldenlau2002@gmail.com>
2021-02-18 02:28:45 -0800

    first commit

---
+++ grouptime.js
@@ -0,0 +1,94 @@
+"use strict"
+let times=new Map()
+function getOverlaps(l1, l2){
+    //Given two sorted arrays of time intervals, return an array of intervals of intersections of the two arrays.
+    //Use two pointers
+    let p1=0;
+    let p2=0;
+    let current=[];
+    let intervals=[];
+    while(p1<l1.length && p2<l2.length){
+        current=[];
+        if(l1[p1][1]<=l2[p2][0]){
+            p1+=1;
+            continue;
+        }
+        if (l2[p2][1]<=l1[p1][0]){
+            p2+=1;
+            continue;
+        }
+        if(l1[p1][0]<l2[p2][0]){
+            current.push(l2[p2][0]);
+        }
+        else{
+            current.push(l1[p1][0]);
+        }
+        if(l1[p1][1]<l2[p2][1]){
+            current.push(l1[p1][1]);
+            p1+=1;
+        }
+        else{
+            current.push(l2[p2][1]);
+            p2+=1;
+        }
+        intervals.push(current)
+    }
+    return intervals;
+}
+
+function addTime(){
+    //first add time to set
+    let name = document.getElementById('name').value;
+    let startTime = document.getElementById('start-time').value;
+    let endTime = document.getElementById('end-time').value;
+    document.getElementById('name').value='';
+    document.getElementById('start-time').value='';
+    document.getElementById('end-time').value='';
+
+    if(times.has(name)){
+        times.get(name).push([parseInt(startTime),parseInt(endTime)]);
+        times.get(name).sort((a,b) => a);
+    }
+    else{
+        times.set(name,[[parseInt(startTime),parseInt(endTime)]]);
+    }
+    let overlaps = null;
+    for(let arr of times){
+        if(overlaps==null){
+            overlaps=arr[1]
+        }
+        else{
+            overlaps=getOverlaps(overlaps,arr[1])
+        }
+    }
+    let overlapNode=document.getElementById('overlap-times');
+    while (overlapNode.firstChild){
+        overlapNode.removeChild(overlapNode.lastChild);
+    }
+    overlaps.sort((a,b) => a);
+    let textNode;
+    let newNode;
+    for(let arr of overlaps){
+        newNode = document.createElement('p');
+        textNode = document.createTextNode('['+arr[0].toString()+', '+arr[1].toString()+']')
+        newNode.append(textNode)
+        overlapNode.append(newNode);
+    }
+    
+}
+document.getElementById('add-time-button').addEventListener('click',addTime);
+/*
+Testing
+let a=[[1,3],[5,9],[10,18]];
+let b=[[2,4],[5,11],[14,19]];
+let c=[[1,5],[6,10],[13,17]];
+let d=[a,b,c]
+for(let arr of getOverlaps(a,b)){
+    console.log(arr[0].toString()+' '+arr[1].toString());
+}
+let e=d[0]
+for(let arr of d.slice(1)){
+    e=getOverlaps(e,arr)
+}
+console.log(e)
+*/
